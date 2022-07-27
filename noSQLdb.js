243234f4a33ef912b5a01926f9045b3af451cf5a
Jmok19927 <jmok19927@gmail.com>
2022-07-27 11:00:59 -0700

    installing dependencies, update git ignore

--- noSQLdb.js
+++ noSQLdb.js
@@ -35,3 +35,6 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 const Question = mongoose.model('Question', questionSchema);
+
+module.exports.Product = Product;
+module.exports.Question = Question;
