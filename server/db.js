26e945d33c36d369bb995fe1abb277eec0532c90
Jmok19927 <jmok19927@gmail.com>
2022-07-28 14:10:44 -0700

    set up db as a pool and server with express

--- server/db.js
+++ server/db.js
@@ -1,10 +1,33 @@
-import postgres from 'postgres';
+const {Pool} = require('pg');
 
-const connection = postgres({
-  PGHOST: process.env.DB_HOST,
-  PGUSER: process.env.DB_USER,
-  PGPASSWORD: process.env.DB_PASSWORD,
-  PGDATABASE: process.env.DB_NAME
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+  max: 50,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 })
 
-export default sql;
+
+
+module.exports = {
+  //adding info to help log
+  query: (text, params, callback) => {
+    const start = Date.now();
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start;
+      //logging info about the query
+      console.log('executed query', { text, params, duration, rows: res.rowCount})
+      callback(err, res)
+    })
+  },
+  //get a client to execute multiple queries
+  getClient: (callback) => {
+    pool.connect((err, client, done) => {
+      callback(err, client, done)
+    })
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require('body-parser');
-const db = require('./db');
+const db = require('./db.js');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/qa/questions', (req,res) => {
+  // base query here, $1 goes into the request parameters.
+  db.query(`SELECT * FROM products WHERE product_id = $1`, [req.params.id], (err, result) => {
+    if (err) {
+      console.log('error at index get questions',err)
+    }
+    //can create/modify the result object.
+    res.send(result.rows[0])
+  })
+})
 
 var port = process.env.PORT || 3000;
 
