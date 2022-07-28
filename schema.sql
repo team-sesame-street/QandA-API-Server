8bf592b310714fb87a767fd45e2d09ae76f5a114
Jmok19927 <jmok19927@gmail.com>
2022-07-27 18:03:18 -0700

    adjusted SQL file to work with postgres

--- schema.sql
+++ schema.sql
@@ -1,14 +1,13 @@
-CREATE DATABASE qanda
+CREATE DATABASE qanda;
 
-USE qanda
+\c qanda
 
 CREATE TABLE products (
-  product_id integer not null auto_increment,
-  primary key (product_id)
+  product_id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE questions (
-  question_id integer not null auto_increment,
+  question_id SERIAL PRIMARY KEY,
   product_id integer not null,
   question_body varchar,
   question_date date,
@@ -16,26 +15,23 @@ CREATE TABLE questions (
   question_helpfulness varchar,
   reported boolean,
   asker_email varchar,
-  primary key (question_id),
   foreign key (product_id) references products (product_id)
 );
 
 CREATE TABLE answers (
-  answer_id integer not null auto_increment,
+  answer_id SERIAL PRIMARY KEY,
   question_id integer not null,
   answer_body varchar,
   answer_date date,
   answerer_name varchar,
   answer_helpfulness varchar,
   answerer_email varchar,
-  primary key (answer_id),
   foreign key (question_id) references questions (question_id)
 );
 
 CREATE TABLE photos (
-  id integer not null auto_increment,
+  id SERIAL PRIMARY KEY,
   answer_id integer not null,
   url varchar,
-  primary key (id),
   foreign key (answer_id) references answers (answer_id)
-)
+);
