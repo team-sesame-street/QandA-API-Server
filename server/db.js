8323f0838072678b4e521dcd8ce3f0293c491d57
Jmok19927 <jmok19927@gmail.com>
2022-07-28 18:51:55 -0700

    working get answers, partial questions, need to wrap in an object

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const {Pool} = require('pg');
+const config = require('../config.js');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: config.DB_HOST,
+  // user: process.env.DB_USER,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
   port: 5432,
   max: 50,
   idleTimeoutMillis: 30000,
@@ -20,7 +22,12 @@ module.exports = {
     return pool.query(text, params, (err, res) => {
       const duration = Date.now() - start;
       //logging info about the query
-      console.log('executed query', { text, params, duration, rows: res.rowCount})
+      console.log('executed query', {
+        text,
+        params,
+        duration,
+        rows: res.rowCount
+      })
       callback(err, res)
     })
   },
--- server/index.js
+++ server/index.js
@@ -7,17 +7,93 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  db.query(
+    `SELECT
+      a.answer_id,
+      a.answer_body body,
+      a.answer_date date,
+      a.answerer_name,
+      a.answer_helpfulness helpfulness,
+      (SELECT json_agg(json_build_object(
+        'id', p.photo_id,
+        'url', p.url
+      ))
+      FROM photos p WHERE p.answer_id = a.answer_id) photos
+
+
+    FROM answers a WHERE a.question_id = ${req.query.question_id}`, [], (err, result) => {
+      if (err) {
+        console.log('error at index get questions',err)
+      }
+      //can create/modify the result object.
+      console.log(result)
+      let resObj = {};
+      resObj.question = req.query.question_id.toString();
+      resObj.page = req.query.page || 0;
+      resObj.count = req.query.count || 5;
+      resObj.results = result.rows;
+
+      res.send(resObj)
+    }
+  )
+})
+
 app.get('/qa/questions', (req,res) => {
-  // base query here, $1 goes into the request parameters.
-  db.query(`SELECT * FROM products WHERE product_id = $1`, [req.params.id], (err, result) => {
+  db.query(
+    `SELECT q.question_id,
+    q.question_body,
+    q.question_date,
+    q.asker_name,
+    q.question_helpfulness,
+    q.reported,
+
+    (SELECT json_agg(json_build_object(
+      'id', a.answer_id,
+      'body', a.answer_body,
+      'date', a.answer_date,
+      'answerer_name', a.answerer_name,
+      'helpfulness', a.answer_helpfulness,
+      'photos',
+      (SELECT json_agg(json_build_object(
+        'id', p.photo_id,
+        'url', p.url
+      ))
+      FROM photos p WHERE p.answer_id = a.answer_id)
+      ))
+
+    FROM answers a WHERE a.question_id = q.question_id) answers
+    FROM questions q WHERE product_id = ${req.query.product_id}`, [], (err, result) => {
     if (err) {
       console.log('error at index get questions',err)
     }
     //can create/modify the result object.
-    res.send(result.rows[0])
+    console.log(result.rows)
+    let resObj = {};
+    resObj.product_id = req.query.product_id.toString();
+    resObj.results = result.rows;
+
+    res.send(resObj)
   })
 })
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {console.log(`Listening at http://localhost:${port}`)});
+
+/*
+    (SELECT json_object_agg(a.answer_id, row_to_json(answers)) FROM (
+      SELECT
+      a.answer_id id,
+      a.answer_body body,
+      a.answer_date date,
+      a.answerer_name,
+      a.answer_helpfulness helpfulness,
+      (SELECT json_agg(json_build_object(
+        'id', p.photo_id,
+        'url', p.url
+      ))
+      FROM photos p WHERE p.answer_id = a.answer_id) photos
+    ))
+
+*/
