e12c65f7f262b2ba368948bf072b98257f7f3d4b
Jmok19927 <jmok19927@gmail.com>
2022-07-29 17:11:53 -0700

    fixing req query vs body vs params

--- server/index.js
+++ server/index.js
@@ -23,14 +23,14 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
       FROM photos p WHERE p.answer_id = a.answer_id) photos
 
 
-    FROM answers a WHERE a.question_id = ${req.query.question_id}`, [], (err, result) => {
+    FROM answers a WHERE a.question_id = ${req.params.question_id}`, [], (err, result) => {
       if (err) {
         console.log('error at index get questions',err)
       }
       //can create/modify the result object.
       console.log(result)
       let resObj = {};
-      resObj.question = req.query.question_id.toString();
+      resObj.question = req.params.question_id.toString();
       resObj.page = req.query.page || 0;
       resObj.count = req.query.count || 5;
       resObj.results = result.rows;
@@ -61,14 +61,14 @@ app.get('/qa/questions', (req,res) => {
       ))
       FROM photos p WHERE p.answer_id = a.answer_id) photos
     FROM answers a WHERE a.question_id = q.question_id) answers ) answers
-    FROM questions q WHERE product_id = ${req.query.product_id}`, [], (err, result) => {
+    FROM questions q WHERE product_id = ${req.params.product_id}`, [], (err, result) => {
     if (err) {
       console.log('error at index get questions',err)
     }
     //can create/modify the result object.
     console.log(result.rows)
     let resObj = {};
-    resObj.product_id = req.query.product_id.toString();
+    resObj.product_id = req.params.product_id.toString();
     resObj.results = result.rows;
 
     res.send(resObj);
@@ -76,7 +76,7 @@ app.get('/qa/questions', (req,res) => {
 })
 
 app.post('/qa/questions', (req, res) => {
-  console.log('post body', req.query);
+  console.log('post body', req.body);
   let timestamp = new Date().toISOString();
   console.log(timestamp);
   db.query(`INSERT INTO questions (
@@ -87,55 +87,59 @@ app.post('/qa/questions', (req, res) => {
     asker_email,
     reported,
     question_helpfulness)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)`, [req.query.product_id, req.query.body, timestamp, req.query.name, req.query.email, false, 0], (err, result) => {
+    VALUES ($1, $2, $3, $4, $5, $6, $7)`, [req.body.product_id, req.body.body, timestamp, req.body.name, req.body.email, false, 0], (err, result) => {
       if (err) {
         console.log('error at index post question', err);
       }
-      console.log('posted item', req.query.product_id, req.query.body, timestamp, req.query.name, req.query.email, false, 0)
+      console.log('posted item', req.body.product_id, req.body.body, timestamp, req.body.name, req.body.email, false, 0)
       res.status(201);
       res.send();
     })
 })
 
+// app.post('/qa/questions/:question_id/answers', (req, res) => {
+//   let timestamp = new Date().toISOString();
+// })
+
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  db.query(`UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = ${req.query.question_id}`, [], (err, result) => {
+  db.query(`UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = ${req.params.question_id}`, [], (err, result) => {
     if (err) {
       console.log('error at index put helpful', err);
     }
-    console.log('updated question helpful', req.query.question_id);
+    console.log('updated question helpful', req.params.question_id);
     res.status(204);
-    res.send(`updated question ${req.query.question_id}`);
+    res.send(`updated question ${req.params.question_id}`);
   })
 })
 
 app.put('/qa/questions/:question_id/report', (req, res) => {
-  db.query(`UPDATE questions SET reported = true WHERE question_id = ${req.query.question_id}`, [], (err, result) => {
+  db.query(`UPDATE questions SET reported = true WHERE question_id = ${req.params.question_id}`, [], (err, result) => {
     if (err) {
       console.log('error at index put report', err);
     }
-    console.log('updated question report', req.query.question_id);
+    console.log('updated question report', req.params.question_id);
     res.status(204);
     res.send();
   })
 })
 
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  db.query(`UPDATE answers SET answer_helpfulness = answer_helpfulness + 1 WHERE answer_id = ${req.query.answer_id}`, [], (err, result) => {
+  db.query(`UPDATE answers SET answer_helpfulness = answer_helpfulness + 1 WHERE answer_id = ${req.params.answer_id}`, [], (err, result) => {
     if (err) {
       console.log('error at index put helpful answer', err);
     }
-    console.log('updated answer helpful', req.query.answer_id);
+    console.log('updated answer helpful', req.params.answer_id);
     res.status(204);
-    res.send(`updated question ${req.query.answer_id}`);
+    res.send(`updated question ${req.params.answer_id}`);
   })
 })
 
 app.put('/qa/answers/:answer_id/report', (req, res) => {
-  db.query(`UPDATE answers SET reported = true WHERE answer_id = ${req.query.answer_id}`, [], (err, result) => {
+  db.query(`UPDATE answers SET reported = true WHERE answer_id = ${req.params.answer_id}`, [], (err, result) => {
     if (err) {
       console.log('error at index put report', err);
     }
-    console.log('updated answer report', req.query.answer_id);
+    console.log('updated answer report', req.params.answer_id);
     res.status(204);
     res.send();
   })
